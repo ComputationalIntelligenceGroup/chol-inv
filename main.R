119192cbcc0905b414ae71a4d65b497265cea507
irenecrsn <icordoba@fi.upm.es>
2019-11-19 13:30:39 +0100

    Update experiment file

--- main.R
+++ main.R
@@ -1,16 +1,35 @@
 source("utils.R")
 
-p <- 10
-d <- 0.25
-N <- 50
+p <- 1000
+d <- 0.0025
 
-dag <- pcalg::randomDAG(n = p, prob = d)
-data <- pcalg::rmvDAG(n = N, dag = dag)
+##### First experiment -> True regression coefficients available
+rep <- 10
+error_solve <- 0
+error_chol <- 0
+for (i in 1:rep) {
+	dag <- pcalg::randomDAG(n = p, prob = d)
+	b_true <- pcalg::wgtMatrix(dag)
+	o_solve <- tryCatch(solve(diag(p) - b_true), error = function(e) {
+			print(e)
+			error_solve <- error_solve + 1
+		})
+	o_chol <- tryCatch(chol_inv(b_true), error = function(e)
+		{
+			print(e)
+			error_chol <- error_chol + 1
+		})
+}
+
+##### Second experiment -> Estimated regression coefficients
+
+#N <- 50
+#dag <- pcalg::randomDAG(n = p, prob = d)
+#data <- pcalg::rmvDAG(n = N, dag = dag)
 
 # Maximum likelihood estimation via regression
-dag_am <- ggm::grMAT(dag)
+#dag_am <- ggm::grMAT(dag)
 
-colnames(data) <- colnames(data, do.NULL = FALSE, prefix = "")
-fit_ggm <- ggm::fitDag(dag_am, cov(data), N)
-
-norms_ggm <- get_norms(U = fit_ggm$Ahat, D = diag(fit_ggm$Dhat), dag = dag)
+#colnames(data) <- colnames(data, do.NULL = FALSE, prefix = "")
+#fit_ggm <- ggm::fitDag(dag_am, cov(data), N)
+#norms_ggm <- get_norms(U = fit_ggm$Ahat, D = diag(fit_ggm$Dhat), dag = dag)

2efa7bb06ceaf1e9399170d7ea36c6ef2599be72
irenecrsn <icordoba@fi.upm.es>
2020-01-15 12:03:21 +0100

    Improve learning experiment. Still not working

--- learn.R
+++ learn.R
@@ -1,5 +1,3 @@
-source("utils.R")
-
 library("ggplot2")
 
 devtools::install_github("irenecrsn/ggmexp")
@@ -9,32 +7,22 @@ r <- 10
 p <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 200, 250, 300, 400, 500, 750, 1000)
 d <- c(0.0025, 0.005, 0.025, 0.05, 0.25, 0.5)
 
-##### First experiment -> True regression coefficients available
-learn_chol_cov <- function(p, d) {
+##### First experiment --> comparison with ggm::fitDag
+learn_chol_conc <- function(p, d, N) {
 	dag <- pcalg::randomDAG(n = p, prob = d)
-	B <- pcalg::wgtMatrix(dag)
-	L <- diag(p) - B
-	res <- tryCatch(
-		{
-			O_inv_solve <- solve(L)
-			O_inv_chol <- covchol::chol_inv(B)	
-			
-			norm(O_inv_chol - O_inv_solve)
-		}, 
-		error = function(e) {
-			print(paste0("Error at p = ", p, " and d = ", d, ":"))
-			print(e)
-			return(NA)
-		})
+	data <- pcalg::rmvDAG(n = N, dag = dag)
+	amat <- t(as(dag, "matrix"))
+	chol_conc_fit <- covchol::fit_chol_conc(amat = amat, data = data)
+	chol_conc_true <- diag(p) - pcalg::wgtMatrix(dag)
 	
-	return(res)
+	return(norm(chol_conc_fit - chol_conc_true))
 }
 
 wd <- getwd()
-plot_dname <- "plot_true"
+plot_dname <- "plot_comp_true"
 dir.create(paste0(wd, "/", plot_dname), showWarnings = FALSE)
-ggmexp::execute(p = p, d = d, r = r, experiment = true)
+ggmexp::execute(p = p, d = d, r = r, experiment = learn_chol_conc, N = 2*p)
 
-pl <- ggmexp::plot_map_reduce(p = p, d = d, r = 1, N = 1, reduce = mean, exp_name = "true") 
+pl <- ggmexp::plot_map_reduce(p = p, d = d, r = 1, N = 1, reduce = mean, exp_name = "comp_true") 
 pl <- pl + ylab("") + ggtitle("")
-ggsave(filename = "true.pdf", plot = pl, device = "pdf", path = plot_dname)
+ggsave(filename = "comp_true.pdf", plot = pl, device = "pdf", path = plot_dname)

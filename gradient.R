0d80159f6941011f7e9ccb8a1f9e2e1c8b586b65
irenecrsn <icordoba@fi.upm.es>
2020-01-31 15:08:40 +0100

    Experiment file

--- gradient.R
+++ gradient.R
@@ -1,34 +1,74 @@
-devtools::install_github("irenecrsn/ggmexp")
 devtools::install_github("irenecrsn/covchol")
 
-r <- 10
+execute_experiment <- function(p, r, ename, emethod, ...) {
+	
+	n_cores <- min(r, parallel::detectCores() - 2)
+	cl <- parallel::makeCluster(n_cores, outfile = "")
+	doParallel::registerDoParallel(cl)
+	
+	iter <- foreach::foreach(repetition = seq(r), .combine = rbind)
+	foreach::"%dopar%"(iter, {
+		dir.create(ename, showWarnings = FALSE)
+		
+		for (nnode in p) {
+			densities <- c(1/nnode, 2/nnode, 3/nnode)
+			for (d in densities) {
+				result <- emethod(p = nnode, d = d, ...)
+				saveRDS(result, file = paste0(ename, "/", nnode, "_", d, "_r", repetition, ".rds")) 
+			}
+		}
+	})
+	
+	parallel::stopCluster(cl)
+}
+
 p <- c(10, 30, 50, 100)
-d <- c(0.0025, 0.005, 0.025, 0.05, 0.25, 0.5)
+r <- 50
+
+#p <- 5; d <- 0.5; n <- 100
 
 ##### Gradient experiment
-gradient <- function(p, d, N) {
+sparse_chol <- function(p, d) {
+	n <- 20*p
+	res <- list()
 	
 	Ltrue <- covchol::rlower(p = p, d = d)
 	diag(Ltrue) <- 1
-	D <-  diag(p)
-	Sigmatrue <- Ltrue %*% D %*% t(Ltrue)
-	
-	X <- MASS::mvrnorm(N, rep(0, p), Sigma = Sigmatrue)
-	Cortrue <- cov2cor(Sigmatrue)
+	Sigmatrue <- Ltrue %*% t(Ltrue)
 
-	Lstart <- t(chol(cor(X)))
-	diag(Lstart) <- 1
+	X <- MASS::mvrnorm(n, rep(0, p), Sigma = Sigmatrue)
+	train <- floor(n/2)
+	Cortrue <- cov2cor(Sigmatrue)
 	
-	Lest <- covchol::proxgradL(Sigma = cor(X), L = Lstart, D = diag(p),
-										lambda = 0.02, h = TRUE, trace = 0, alpha = 1, beta = 0.5, 
-										maxIter = 100, eps = 0.0001)
+	Cortrain <- cor(X[1:train, ])
+
+	llpath <- covchol::llpathL(Sigma = Cortrain)
+	frobs <- lapply(X = llpath, FUN = function(res) {
+		ldebias <- covchol::proxgradL(Sigma = Cortrain, L = res$L, lambda = 0, eps = 1e-10)
+		norm(ldebias$L %*% t(ldebias$L) - cor(X[(train + 1):n, ]), type = "F")
+	})
+
+	Lest <- llpath[[which.min(frobs)]]$L
 	Corest <- Lest %*% t(Lest)
-	res_mll <- covchol::mll(solve(Corest), Cortrue)
-	min_mll <- covchol::mll(solve(Cortrue), Cortrue)
 	
-	
-	return(list("res_mll" = res_mll, "min_mll" = min_mll,
-							"ltrue" = Ltrue, "lest" = Lest))
+	return(list("ltrue" = Ltrue, "lest" = Lest))
 }
 
-ggmexp::execute_experiment(p = p, d = d, r = r, N = 10*p, ename = "gradient", emethod = gradient)
+band_chol <- function(p, d) {
+	n <- 2*p
+	res <- list()
+	
+	Ltrue <- covchol::rlower(p = p, d = d)
+	diag(Ltrue) <- 1
+	Sigmatrue <- Ltrue %*% t(Ltrue)
+	
+	X <- MASS::mvrnorm(n, rep(0, p), Sigma = Sigmatrue)
+	
+	Sigmaest <- PDSCE::band.chol.cv(x = X)$sigma
+	Lest <- zapsmall(t(chol(Sigmaest)))
+		
+	return(list("ltrue" = Ltrue, "lest" = Lest))
+}
+
+execute_experiment(p = p, r = r, ename = "sparse_chol", emethod = sparse_chol)
+execute_experiment(p = p, r = r, ename = "band_chol", emethod = band_chol)

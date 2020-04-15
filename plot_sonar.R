5c88e09d6bfc7f014129e2f180a77b5ba8d55346
irenecrsn <icordoba@fi.upm.es>
2020-04-15 16:01:52 +0200

    Add pred results code. Fix scree

--- plot_sonar.R
+++ plot_sonar.R
@@ -29,7 +29,8 @@ eigens <- array(dim = c(length(type), length(est), 60),
 			
 for (t in type) {
 	for (e in est) {
-		eigens[t, e, ] <- eigen(covs[t, e, , ])$values
+		eigens[t, e, ] <- eigen(covs[t, e, , ], symmetric = TRUE, 
+			only.values = TRUE)$values
 	}
 }
 
@@ -37,13 +38,54 @@ df <- eigens %>% as.tbl_cube(met_name = "eigens") %>% as_tibble()
 df$type <- as.factor(df$type)
 df$est <- as.factor(df$est)
 
-pl <- ggplot(df, aes(eigens, group = est, color = est)) +
+pl <- ggplot(df, aes(x = value, y = eigens, group = est, color = est)) +
 	facet_grid(cols = vars(type)) +
-	geom_freqpoly(bins = nclass.FD(df$eigens)) +
+	geom_line() + coord_fixed(ratio = 30) +
 	theme_bw() +
-	xlab("Eigenvalue") +
-	ylab("Absolute frequency") 
+	theme(legend.position = "bottom") +
+	xlab("") +
+	ylab("Eigenvalue") 
 
 ggsave(filename = paste0("sonar_eigens.pdf"), plot = pl, device = "pdf",
 	path = "../sparsecholeskycovariance/img/")
+
+
+preds <- readRDS(file = "data/preds.rds")
+stat_tpr <- function(pred, true, val) {
+	tp <- sum(true == val & pred == val)
+	return(tp/(sum(true == val)))
+}
+stat_tnr <- function(pred, true, val) {
+	if (sum(true != val) == 0) {
+		return(1)
+	}
+	tn <- sum(true != val & pred != val)
+	return(tn/sum(true != val))
+}
+stat_f1 <- function(pred, true, val) {
+	tp <- sum(true == val & pred == val)
+	fn <- sum(pred != val & true == val)
+	fp <- sum(true != val & pred == val)
+	return(2*tp/(2*tp + fp + fn))
+}
+
+fstat <- c("tpr" = stat_tpr,
+			"tnr" = stat_tnr,
+			 "f1" = stat_f1)
+stat <- array(
+	dim = c(length(type), length(fstat), length(est)),
+	dimnames = list(type = type, fstat = names(fstat), est = est)
+)
+
+for (t in type) {
+	for (e in est) {
+		for (f in fstat) {
+			stat[t, f, e] <- fstat[[f]](pred = preds[, e], 
+										true = preds[, "true"], 
+										val = t)
+		}
+	}
+}
+
+saveRDS(stat, file = "data/stat_preds.rds")
 
